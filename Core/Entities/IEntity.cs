3a14e38eda943b0b7f89294ecd8d71c92d8dc2ba
Mert Çakır <mert__63@windowslive.com>
2021-02-07 22:46:38 +0300

    7 Şubat Proje Devamı

--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
     public interface IEntity
         //IEntitiy implement eden class bir veritabanı tablosudur
